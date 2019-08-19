5e0aa8987cd2db6ad57ae8d32fd9d8ae624e5f47
hn4729 <henry0118@utexas.edu>
2019-08-19 09:31:30 -0500

    working on hosting still

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import * as serviceAccount from "./serviceAccount.json";
 // import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import store from "./redux/store";
@@ -10,13 +11,26 @@ import { RestLink } from "apollo-link-rest";
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloProvider } from "react-apollo";
+import { setContext } from "apollo-link-context";
 
 const restLink = new RestLink({
   uri: "https://api.pandascore.co"
 });
 
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = serviceAccount.pandascore_key;
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ""
+    }
+  };
+});
+
 const client = new ApolloClient({
-  link: restLink,
+  link: authLink.concat(restLink),
   cache: new InMemoryCache(),
   fetchOptions: {
     mode: "no-cors"
